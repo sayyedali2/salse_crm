b1d2caca5518a9e30f9dd97c30a6e258aa40f850
Sayyed Amaan Ali <sayyedamaanali164@gmail.com>
2025-12-25 23:53:30 +0530

    commit

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -7,8 +7,8 @@ export class MailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
